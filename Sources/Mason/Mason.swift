3ebc08f553ff4542ee90d3a51f18bde779d4828f
Mason Mark <mason@fivespeed.com>
2018-07-23 10:21:23 +0900

    Remove class with same name as package (since that causes problems)

--- Sources/Mason/Mason.swift
+++ Sources/Mason/Mason.swift
@@ -1,28 +1,21 @@
 import Foundation
 
-public class Mason {
-    
-    func fu(_ bro: String = "🌎") {
-        print("F.U., \(bro).")
-    }
-    
-    
-    /// Make prettier strings for printing to console.
-    
-    public class func stringify(_ what: Any?) -> String {
-        if let str = what as? String {
-            return str
-        } else {
-            return "\(String(describing: what))"
-        }
-    }
 
+public func stringify(_ what: Any?) -> String {
+    
+    if let str = what as? String {
+        return str
+    } else {
+        return "\(String(describing: what))"
+    }
 }
 
+
 public func todo(_ what: Any? = nil, function: String = #function, file: String = #file, line: Int = #line) {
+    
     var msg = "ℹ️ → TODO: "
     if what != nil {
-        msg += Mason.stringify(what)
+        msg += stringify(what)
     }
     
     log(msg, function: function, file: file, line: line)
@@ -30,11 +23,12 @@ public func todo(_ what: Any? = nil, function: String = #function, file: String 
 
 
 public func log(_ what: Any? = nil, function: String = #function, file: String = #file, line: Int = #line) {
+    
     let fileName = URL(fileURLWithPath: file).lastPathComponent
     
     var msg = ""
     if what != nil {
-        msg += Mason.stringify(what)
+        msg += stringify(what)
         msg += "  —  "
     }
     msg += "\(function), \(fileName):\(line)"
